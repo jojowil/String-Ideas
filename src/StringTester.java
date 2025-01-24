c81914abaa05e4d6f139b68599900515aa0f654b
Bill Jojo <jojowil@gmail.com>
2025-01-23 21:14:09 -0500

    more test

--- src/StringTester.java
+++ src/StringTester.java
@@ -2,7 +2,18 @@ public class StringTester {
 
     public static void main(String[] args) {
 
+        MyString s = new MyString("My really cool content");
 
-        System.out.println();
+        System.out.println(s.length() + " " + s);
+        System.out.println(s.indexOf('!'));
+        System.out.println(s.indexOf('M'));
+        System.out.println(s.indexOf("cool"));
+        System.out.println(s.indexOf("My"));
+        System.out.println(s.indexOf("tent"));
+
+        MyString t = new MyString("acbd1234");
+        MyString u = s.concat(t);
+        System.out.println(u.length() + " " + u);
+
     }
 }
